3cbc9e5ac3d78cc2f47bdfcee794a8a9ee5d2611
kryssel tillada <krysseltillada@gmail.com>
2015-08-31 20:08:44 +0800

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,47 +1,37 @@
 #ifndef GAME_HEADER
 #define GAME_HEADER
 
-#include <conio.h> /// _getch ()
-#include <cstdlib> /// rand()
-#include <string> /// std::string
-#include <iostream> /// std::cout; std::endl;
-
-/// interface file
+#include <conio.h>
+#include <cstdlib>
+#include <string>
+#include <iostream>
 
 class Game {
 public:
-    void set_name (std::string &);
     void draw ();
-    void reset_score ();
-    void reset ();
-    void rand_spawn_bait ();
-    void reset_spawn_snake ();
-    void update_world ();
-    void draw_world ();
-    void check_collision (char [][70], size_t &, size_t &);
-    void input ();
+    void update ();
+    void event ();
     void Clear ();
-    void GameOver ();
-    void run ();
 
-    bool Game_state ();
+    bool GameState ();
 
 private:
+    bool if_GameOver = false;
     std::string name;
 
-    static bool state;
-    static constexpr size_t height = 20, width = 70;
-    unsigned score = 0;
+    int taily[100], tailx[100], ntail = 0;
 
-    char world [height][width];
-    char world_bounds[height][width];
+    int height = 20, width = 30, score = 0;
+    int snake_y = height / 2, snake_x = width / 2, fruit_y = rand() % height, fruit_x = rand() % width;
 
-    int snake_x = width / 2, snake_y = height / 2;
-    int range_y = 6, range_x = 10;
+    enum KeyInput{
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    };
 
-    int bait_x = rand() % (width - range_x), bait_y = rand() % (height - range_y);
-    int velocity = 1;
+    KeyInput key;
+
 };
-
-
 #endif // GAME_HEADER
