8b4267304ac778dd63b41757ad54ecfb311d6162
kryssel tillada <krysseltillada@gmail.com>
2015-08-31 20:09:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
-#include <iostream>
 #include "Game.h"
 
 int main()
 {
     Game game;
-    std::string input;
 
-    std::cout << "snake game:" << std::endl
-              << "----------"  << std::endl;
-
-    std::cout << "enter your name: " << std::endl;
-
-    std::getline (std::cin, input);
-
-    game.set_name (input);
-
-    while (game.Game_state()) { /// checks the game state
-        game.run (); /// runs the  game
-    }
+    while (!game.GameState()) {
+        game.draw ();
+        game.event ();
+        game.update ();
+        game.Clear ();
+     }
 
     return 0;
 }
